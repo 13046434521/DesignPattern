727607118a15cb7b40f07dde4d6f6698f08d131f
龙昊 <604872657@qq.com>
2021-08-09 17:29:57 +0800

    策略模式

---
+++ src/strategy/Comparator.java
@@ -0,0 +1,10 @@
+package strategy;
+
+/**
+ * @author jtl
+ * @date 2021/8/9 15:59
+ */
+
+interface Comparator<T> {
+    int compare(T t1,T t2);
+}
---
+++ src/strategy/Dog.java
@@ -0,0 +1,21 @@
+package strategy;
+
+/**
+ * @author jtl
+ * @date 2021/8/9 16:12
+ */
+
+class Dog {
+   float food;
+
+   public Dog(float food) {
+      this.food = food;
+   }
+
+   @Override
+   public String toString() {
+      return "Dog{" +
+              "food=" + food +
+              '}';
+   }
+}
