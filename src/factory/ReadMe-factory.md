e6910ee6e512ac88f1f4085223e285d5a8bc5309
龙昊 <604872657@qq.com>
2023-02-06 21:49:17 +0800

    添加工厂模式的注释

---
+++ src/factory/ReadMe-factory.md
@@ -0,0 +1,12 @@
+# ReadMe
+
+## common包
+    正常的工厂模式
+## reflect包
+    反射的工厂模式
+
+## common包
+
+## common包
+
+## common包
--- src/factory/simple/reflect/Factory.java
+++ src/factory/simple/reflect/Factory.java
@@ -6,6 +6,12 @@ package factory.simple.reflect;
  * @detail：简单工厂类
  */
 class Factory {
+    /**
+     * 通过反射的方式创建对象。通过泛型T来确定具体类型
+     * @param clazz CLASS类
+     * @return 返回值
+     * @param <T> 泛型 T
+     */
     public static  <T extends Product> T createProduct(Class<T> clazz){
         T product=null;
         try {
