7a2124e32003fad6600356fedcb6c1a8d1f3c92e
龙昊 <604872657@qq.com>
2023-07-11 23:21:06 +0800

    Changes

--- src/proxy/dynamicproxy/Client.java
+++ src/proxy/dynamicproxy/Client.java
@@ -13,38 +13,70 @@ public class Client {
     public static void main(String[] args) {
         FaceSession faceSession = new FaceSession();
 
-
-
-        IFaceDetect faceDetect = (IFaceDetect) Proxy.newProxyInstance(IFaceDetect.class.getClassLoader(), new Class[]{IFaceDetect.class}, new InvocationHandler() {
+        IFaceDetect permissionFaceDetect = (IFaceDetect) Proxy.newProxyInstance(IFaceDetect.class.getClassLoader(), new Class[]{IFaceDetect.class}, new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
                 Object obj = null;
-                if (method.getName().equals("faceLiveness")) {
-                    System.out.println("申请相机权限");
-                    long tt = System.currentTimeMillis();
-                    obj = method.invoke(faceSession, args);
-                    System.out.println("faceLiveness方法耗时：" + (System.currentTimeMillis() - tt));
-                } else if (method.getName().equals("faceQuality")) {
+                switch (method.getName()) {
+                    case "faceLiveness":
+                    case "faceQuality":
+                        System.out.println("申请相机权限");
+                        obj = method.invoke(faceSession, args);
+                        break;
 
-                    System.out.println("申请相机权限");
-                    long tt = System.currentTimeMillis();
-                    obj = method.invoke(faceSession, args);
-                    System.out.println("faceQuality方法耗时：" + (System.currentTimeMillis() - tt));
+                    case "faceDetect":
+                        System.out.println("申请网络权限");
 
-                } else if (method.getName().equals("faceDetect")) {
+                        obj = method.invoke(faceSession, args);
 
-                    System.out.println("申请相机权限");
-                    long tt = System.currentTimeMillis();
-                    obj = method.invoke(faceSession, args);
-                    System.out.println("faceDetect方法耗时：" + (System.currentTimeMillis() - tt));
+                        break;
+                    default:
 
-                } else {
-
+                        break;
                 }
                 return obj;
             }
         });
 
-        faceDetect.faceLiveness();
+
+        IFaceDetect timeFaceDetect = (IFaceDetect) Proxy.newProxyInstance(IFaceDetect.class.getClassLoader(), new Class[]{IFaceDetect.class}, new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                Object obj = null;
+                switch (method.getName()) {
+                    case "faceLiveness": {
+
+                        long tt = System.currentTimeMillis();
+                        obj = method.invoke(faceSession, args);
+                        System.out.println("faceLiveness方法耗时：" + (System.currentTimeMillis() - tt));
+                        break;
+                    }
+                    case "faceQuality": {
+
+                        long tt = System.currentTimeMillis();
+                        obj = method.invoke(faceSession, args);
+                        System.out.println("faceQuality方法耗时：" + (System.currentTimeMillis() - tt));
+
+                        break;
+                    }
+                    case "faceDetect": {
+
+                        long tt = System.currentTimeMillis();
+                        obj = method.invoke(faceSession, args);
+                        System.out.println("faceDetect方法耗时：" + (System.currentTimeMillis() - tt));
+
+                        break;
+                    }
+                    default:
+
+                        break;
+                }
+                return obj;
+            }
+        });
+
+        permissionFaceDetect.faceLiveness();
+
+        timeFaceDetect.faceQuality();
     }
 }
